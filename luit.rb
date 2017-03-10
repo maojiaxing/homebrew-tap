8b10160d262cb965e49c3eff464de450b88318aa
maojiaxing <maojiaxing.s@gmail.com>
2017-03-10 17:01:38 +0800

    Update luit.rb
    format

--- luit.rb
+++ luit.rb
@@ -2,7 +2,7 @@ class Luit < Formula
 	desc "Luit is a filter that can be run between an arbitrary application and a UTF-8 terminal emulator. It will convert application output from the locale's encoding into UTF-8, and convert terminal input from UTF-8 into the locale's encoding."
 	homepage "http://invisible-island.net/luit/luit.html"
 	url "http://invisible-island.net/datafiles/release/luit.tar.gz"
-  version "1.0.0"
+  	version "1.0.0"
 
 	def install
 		system "./configure", "--prefix=#{prefix}"
